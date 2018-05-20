fc7bbafc3e5ed5860d7f1b0e85331b830bd2efa2
dmsch <dschukin@mail.ru>
2018-05-20 16:33:38 +0300

    message api

---
+++ api/models/Message.js
@@ -0,0 +1,49 @@
+/**
+ * Message.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: 'message',
+  migrate: 'safe',
+  attributes: {
+    guid: {
+      type: 'string',
+      size: 36,
+    },
+    message: {
+      type: 'string',
+      size: 255,
+    },
+    message_format: {
+      type: 'string',
+      size: 50,
+    },
+    message_buttons: {
+      type: 'json',
+    },
+    messenger: {
+      type: 'string',
+      size: 36,
+    },
+    status: {
+      type: 'string',
+      size: 36,
+    },
+    status_text: {
+      type: 'string',
+      size: 100,
+    },
+    type: {
+      type: 'string',
+      size: 36,
+    },
+    message_originator: {
+      type: 'string',
+      size: 36,
+    },
+  }
+};
+
