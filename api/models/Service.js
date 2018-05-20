c4380f7f482f599121b89647a239ddd38ead46ae
dmsch <dschukin@mail.ru>
2018-05-20 18:18:45 +0300

    service api

---
+++ api/models/Service.js
@@ -0,0 +1,31 @@
+/**
+ * Service.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: 'service',
+  migrate: 'safe',
+  attributes: {
+    guid: {
+      type: 'string',
+      size: 36,
+    },
+    name: {
+      type: 'string',
+      size: 20,
+    },
+    rooms: {
+      type: 'integer',
+    },
+    messages: {
+      type: 'integer',
+    },
+    messages_to_stars: {
+      type: 'integer',
+    },
+  }
+};
+
